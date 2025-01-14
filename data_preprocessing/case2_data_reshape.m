7c54e678c6b3e465f3efc040f0d96991aa917f33
hannah-yun <hyun536@aucklanduni.ac.nz>
2025-01-14 19:00:31 +0900

    Add files via upload

---
+++ data_preprocessing/case2_data_reshape.m
@@ -0,0 +1,26 @@
+%%%% data preparation to apply the file to the algorithm
+
+% Step 1: Get list of all files in the folder
+folder_path = cd; % Replace with your folder path
+files = dir(fullfile(folder_path, '*.mat')); % Assuming .mat files; modify for other formats
+
+sig_segments = []; % Initialise empty matrix for column binding 
+
+% Step 2: Loop through each file
+for i = 1:length(files)
+    % Load the file
+    file_path = fullfile(folder_path, files(i).name);
+    data = load(file_path); % Assuming the file contains a variable with accH column
+
+    % Step 3: Extract accH column (acceleration in horizontal direction)
+    accH = data.accH; 
+
+    % Step 4: Reshape the data
+    reshaped_data = reshape(accH, 8192, 75); % Reshape to 8192 × 75
+
+    % Step 5: Column bind
+    sig_segments = [sig_segments, reshaped_data]; % Append reshaped data horizontally
+end
+
+% Step 6: Save the result 
+save('sig_segments.mat', 'sig_segments');
---
+++ data_preprocessing/case3_welch_method.m
@@ -0,0 +1,55 @@
+
+    % Parameters
+    Fs =12800;  %Sampling frequency (Hz)
+    L = 16384;  %Length of signal
+
+    % Load the data
+    raw_data = 'filtered_T5.mat';
+    data = load(raw_data);
+
+    Y = data.filter_mtx;
+
+   % Y = filter_mtx;
+    x_input = Y; % Assuming you are interested in the first three columns
+    
+    % Define the frequency vector for PSD
+    [~, f] = pwelch(x_input(:,1), 1024,512,1024, Fs);
+    itr = size(x_input, 2);
+
+    % Initialize the matrix
+    psd_welch = zeros(length(f), itr + 1);
+
+    % Assign the frequency vector to the first column
+    psd_welch(:, 1) = f;
+
+    for i = 1:itr
+        signal = x_input(:, i);
+
+        % Check if the length of signal matches L
+        if length(signal) ~= L
+            error('Length of signal does not match L');
+        end
+
+        % Compute the PSD using pwelch
+        [Pxx, ~] = pwelch(signal, 1024,512,1024, Fs);
+
+        % Store the PSD values in the corresponding column of the matrix
+        psd_welch(:, i + 1) = Pxx;
+
+        % Print the iteration number every 50th loop
+        if mod(i, 50) == 0
+            fprintf('Iteration number: %d\n', i);
+        end    
+    
+    end % loop
+
+    % Data slicing
+    % find the indices where the fequency is less than or equal to 1000Hz
+    inds = psd_welch(:,1) <= 1000;
+
+    % slice the dataset to keep rows where the frequency is between 0 and 1000
+    slice_data = psd_welch(inds,:);
+
+    % Save sliced_data to .mat file
+    save("case3_welch_T5.mat", 'slice_data');
+
